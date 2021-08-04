4c417297b841f07978ae9fab94009cf0ae0ba24a
Kamil Kowalski <kamil@kamilkowalski.pl>
2021-08-04 17:21:35 +0200

    Configure package for publishing

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule PrivatePackage.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "A test private package",
+      package: package()
     ]
   end
 
@@ -21,8 +23,14 @@ defmodule PrivatePackage.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      organization: "renovate_test",
+      licenses: ["MIT"]
     ]
   end
 end
