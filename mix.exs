2e9928f6cd0c638e51887ad34d0e47c70548b721
Kamil Kowalski <kamil@kamilkowalski.pl>
2021-08-08 19:07:53 +0200

    Release 0.1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PrivatePackage.MixProject do
   def project do
     [
       app: :private_package,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
